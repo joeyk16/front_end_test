56d3b34c7cc645a4114a76b9fee3a7c9244c77a7
Joseph Konop <joseph@netengine.com.au>
2018-02-22 19:35:48 +1000

    index complete

--- src/Main.js
+++ src/Main.js
@@ -1,49 +1,75 @@
 import React, { Component } from "react";
+import deliveriesService from "./deliveriesService.js";
 
 class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      deliveries: []
+    };
+  }
+
+  componentWillMount() {
+    this.deliveriesService = new deliveriesService()
+    this.deliveries()
+  }
+
+  deliveries() {
+    this.deliveriesService.index()
+    .then((res) => {
+      this.setState({
+        deliveries: res,
+      });
+    })
+  }
+
   render() {
+    const deliveries = this.state.deliveries
+
     return (
-        <div class="container">
-            <header class="header clearfix">
-                <nav>
-                    <ul class="nav nav-pills float-right">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="/">Deliveries</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">New Delivery</a>
-                        </li>
-                    </ul>
-                </nav>
-                <h3 class="text-muted">CartonCloud</h3>
-            </header>
-            <main role="main">
-                <h1>Deliveries</h1>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Driver</th>
-                            <th scope="col">&nbsp;</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                          <th scope="row">1</th>
-                          <td>test</td>
-                          <td>test</td>
-                          <td>test</td>
-                          <td class="text-right">
-                              <a class="btn btn-outline-primary" href="/">Edit</a>
-                              <a class="btn btn-outline-danger" href="/">Delete</a>
-                          </td>
-                      </tr>
-                    </tbody>
-                </table>
-            </main>
-        </div>
+      <div className="container">
+        <header className="header clearfix">
+          <nav>
+            <ul className="nav nav-pills float-right">
+              <li className="nav-item">
+                <a className="nav-link active" href="/">Deliveries</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">New Delivery</a>
+              </li>
+            </ul>
+          </nav>
+          <h3 className="text-muted">CartonCloud</h3>
+        </header>
+        <main role="main">
+          <h1>Deliveries</h1>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Date</th>
+                <th scope="col">Name</th>
+                <th scope="col">Driver</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {deliveries.map(delivery =>
+                <tr>
+                  <th scope="row">{delivery.id}</th>
+                  <td>{delivery.pick_up_date}</td>
+                  <td>{delivery.name}</td>
+                  <td>{delivery.driver.first_name}</td>
+                  <td className="text-right">
+                    <a className="btn btn-outline-primary" href="/">Edit</a>
+                    <a className="btn btn-outline-danger" href="/">Delete</a>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </main>
+      </div>
     );
   }
 }
