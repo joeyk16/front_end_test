dce05dd6aae65f2d1377e98b12eabb7ee7c4f0bc
joeyk16 <joeyk16@users.noreply.github.com>
2018-02-28 09:22:28 +1000

    spelling

--- src/services/driversService.js
+++ src/services/driversService.js
@@ -1,4 +1,4 @@
-// This should be an enivrontment variable for simplicity I will leave it
+// This should be an environment variable, for simplicity I will leave it.
 const baseUrl = 'https://carton-cloud-api.herokuapp.com/'
 
 export default class driversService {
